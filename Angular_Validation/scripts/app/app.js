d42483631849b6c9dd36d0b2a3b5ab82e68f3afb
Sanket <sanket.chaudhary@gmail.com>
2018-01-15 01:07:33 +1100

    Angular form validation files

---
+++ Angular_Validation/scripts/app/app.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../angular.js" />
+
+// create angular app
+var validationApp = angular.module('validationApp', []);
+
+// create angular controller
+validationApp.controller('mainController', function ($scope) {
+
+    // function to submit the form after all validation has occurred            
+    $scope.submitForm = function (isValid) {
+
+        // check to make sure the form is completely valid
+        if (isValid) {
+            alert('Our form is amazing');
+        }
+    };
+});
+
